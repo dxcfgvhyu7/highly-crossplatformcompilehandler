0dbd6389325a38f495ea098a100ba868f82ddda3
dxcfgvhyu7 <gs1kornskie@hotmail.com>
2024-05-11 00:32:28 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-function coinChange(coins, amount) {
-  const dp = new Array(amount + 1).fill(Infinity);
-  dp[0] = 0;
-  for (const coin of coins) {
-    for (let i = coin; i <= amount; i++) {
-      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-    }
-  }
-  return dp[amount] === Infinity ? -1 : dp[amount];
-}
+const deepFlatten = (arr) =>
+  [].concat(...arr.map((v) => (Array.isArray(v) ? deepFlatten(v) : v)));
